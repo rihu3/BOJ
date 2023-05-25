0f0d3d89f1d7dfcfa587acec4ac69acf753417b1
RIHU <132729002+rihu3@users.noreply.github.com>
2023-05-25 15:50:29 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define MAX 92
+
+int main(void)
+{
+	int N;
+	scanf("%d", &N);
+
+	long long num[MAX] = { 0 };
+	num[0] = 1;
+	num[1] = 1;
+	for (int i = 2; i < N; i++) {
+		num[i] = num[i - 2] + num[i - 1];
+	}
+	printf("%lld", num[N - 1]);
+
+	return 0;
+}
