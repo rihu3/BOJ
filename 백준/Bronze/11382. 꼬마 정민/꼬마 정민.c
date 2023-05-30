89d37af9a7c5177a14a0d3de3126e0a28dd1eecd
RIHU <132729002+rihu3@users.noreply.github.com>
2023-05-30 13:28:54 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    
+    long long a, b, c;
+    scanf("%lld %lld %lld", &a, &b, &c);
+    long long sum = a + b + c;
+    printf("%lld",sum);
+    
+    return 0;
+}
