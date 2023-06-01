84373888e7f47f267e8e816f251e2d90ca7ba4e8
RIHU <132729002+rihu3@users.noreply.github.com>
2023-06-01 17:28:07 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    
+    for(int i = 0; i < N; i++)
+    {
+        for(int j=0; j<i+1; j++)
+        {
+         if(j == i) printf("%c\n",'*');
+         else printf("%c",'*');
+        }
+    }
+}
