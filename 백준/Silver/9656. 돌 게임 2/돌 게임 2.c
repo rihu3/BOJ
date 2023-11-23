617f606295953f5d4350b78207f192ef22a64197
RIHU <rihu0311@gmail.com>
2023-11-24 03:04:13 +0900

    [Silver IV] Title: 돌 게임 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+main()
+{
+	int n;
+	scanf("%d", &n);
+	if (n % 2 != 0)
+	{
+		printf("%s", "CY");
+	}
+	else
+	{
+		printf("%s", "SK");
+	}
+	
+	return 0;
+}
