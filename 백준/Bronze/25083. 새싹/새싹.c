cde02ac39605909d4d7941026e3dad96d126e199
RIHU <rihu0311@gmail.com>
2023-11-24 03:09:37 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("         ,r'\"7\n");
+    printf("r`-_   ,'  ,/\n");
+    printf(" \\. \". L_r\'\n");
+    printf("   \`~\\/\n");
+    printf("      |\n");
+    printf("      |\n");
+
+    return 0;
+}
