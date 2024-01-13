89e247673e112f42a42000fb4f8d96ab1bc7790e
RIHU <rihu0311@gmail.com>
2024-01-13 17:38:26 +0900

    [Bronze I] Title: 단어 공부, Time: 200 ms, Memory: 146608 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,12 @@
+word = input().upper()     
+word_list = list(set(word)) 
+cnt = []
+
+for i in word_list:        
+    count = word.count(i)
+    cnt.append(count)       
+
+if cnt.count(max(cnt)) >= 2:
+    print("?")
+else: 
+    print(word_list[(cnt.index(max(cnt)))])
