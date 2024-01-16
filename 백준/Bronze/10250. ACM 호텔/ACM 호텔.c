006404383bad878b8a8e0377e0443d35c00d9277
RIHU <rihu0311@gmail.com>
2024-01-16 10:52:47 +0900

    [Bronze III] Title: ACM 호텔, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.c
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.c
@@ -4,24 +4,17 @@ int main()
 {
     int num;
     scanf("%d", &num);
-
     for (int i = 0; i < num; i++)
     {
         int H, W, N;
         scanf("%d %d %d", &H, &W, &N);
-
         int floor = N % H;
+        int room = (N / H) + 1;
         if (floor == 0) {
             floor = H;
+            room -= 1;
         }
-
-        int room = (N / H) + 1;
-       if (N % H == 0) {
-			room -= 1;
-		}
-
         printf("%d%02d\n", floor, room);
     }
-
     return 0;
 }
