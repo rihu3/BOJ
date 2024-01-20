7357da5dda265cbc3c7ddad10f6223accc80904d
RIHU <rihu0311@gmail.com>
2024-01-21 06:15:16 +0900

    [Bronze I] Title: 팰린드롬수, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.c
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.c
@@ -1,31 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
 	while (1)
 	{
 		char num[5];
 		scanf("%s", num);
-		if (num[0] == '0') break;
-		int idx = 0;
-		while (1) // 길이 확인
+		if (num[0] == '0') break; // 0이면 종료
+		int len = strlen(num);
+		int flag = 1; //true
+		for (int i = 0; i < len / 2; i++)
 		{
-			if (num[idx] == NULL) break;
-			idx++;
-		}
-		if (idx == 1)
-		{
-			printf("yes\n");
-			continue;
-		}
-		for (int i = 0; i < idx / 2; i++)
-		{
-			if (num[i] != num[idx - i - 1])
+			if (num[i] != num[len - i - 1])
 			{
-				printf("no\n");
+				flag = 0; //false
 				break;
 			}
-			if ((i == (idx / 2) - 1)) printf("yes\n");
 		}
-
+		if (flag == 1) printf("yes\n");
+		else printf("no\n");
 	}
 }
