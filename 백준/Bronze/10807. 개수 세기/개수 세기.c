0cfef926ba7313a89a76b17e1797278dc38294d4
RIHU <rihu0311@gmail.com>
2024-01-28 05:13:26 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	int nums[100];
+	for (int i = 0; i < N; i++)
+	{
+		scanf("%d", &nums[i]);
+	}
+	int v;
+	scanf("%d", &v);
+	int cnt = 0;
+	for (int j = 0; j < N; j++)
+	{
+		if (nums[j] == v)
+			cnt++;
+	}
+	printf("%d", cnt);
+	return 0;
+
+
+}
