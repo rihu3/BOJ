03db0c114c6f01fc0bb3d86097c96566fb40aa50
RIHU <rihu0311@gmail.com>
2024-02-12 05:20:20 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 64 ms, Memory: 2560 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct point {
+    int x;
+    int y;
+};
+
+int compare(const void* a, const void* b) {
+    struct point* p1 = (struct point*)a;
+    struct point* p2 = (struct point*)b;
+    
+    if (p1->y != p2->y) {
+        return p1->y - p2->y;
+    } else {
+        return p1->x - p2->x;
+    }
+}
+
+int main(void) {
+    struct point ary[100000];
+    int N;
+    scanf("%d", &N);
+    
+    for (int i = 0; i < N; i++) {
+        scanf("%d %d", &ary[i].x, &ary[i].y);
+    }
+    
+    qsort(ary, N, sizeof(struct point), compare);
+    
+    for (int i = 0; i < N; i++) {
+        printf("%d %d\n", ary[i].x, ary[i].y);
+    }
+    
+    return 0;
+}
