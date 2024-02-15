2c09900caa3c1a09db22b7a1e2f2cba44c13b7cb
RIHU <rihu0311@gmail.com>
2024-02-16 00:29:22 +0900

    [Silver IV] Title: 제로, Time: 28 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Silver IV/10773. 제로/제로.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int k;
+	cin >> k;
+	int stack[100001];
+	int top = -1;
+	for (int i = 0; i < k; i++)
+	{
+		int x;
+		cin >> x;
+		if (x == 0)
+		{
+			top--;
+		}
+		else
+		{
+			top++;
+			stack[top] = x;
+		}
+	}
+	int sum = 0;
+	for (int j = 0; j <= top; j++) {
+		sum += stack[j];
+	}
+	cout << sum;
+	return 0;
+}
