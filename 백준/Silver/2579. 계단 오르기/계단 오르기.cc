e14f5b4dd3239de28c8df4eaa536316524f274a6
RIHU <rihu0311@gmail.com>
2024-02-20 20:31:22 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int Max(int a, int b)
+{
+	return a > b ? a : b;
+}
+int main()
+{
+	int n, stair[300], dp[300];
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> stair[i];
+	dp[0] = stair[0];
+	dp[1] = stair[0] + stair[1];
+	dp[2] = Max(stair[0] + stair[2], stair[1] + stair[2]);
+
+	for (int i = 3; i < n; i++)
+	{
+		dp[i] = Max(dp[i - 2] + stair[i], dp[i - 3] + stair[i - 1] + stair[i]);
+	}
+	cout << dp[n - 1];
+	return 0;
+}
