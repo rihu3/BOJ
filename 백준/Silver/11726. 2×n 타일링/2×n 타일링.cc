3e21cc9a1bc895458d256119f63e699378a8f84b
RIHU <rihu0311@gmail.com>
2024-03-07 00:47:54 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int dp[1001];
+int main()
+{
+	int n;
+	cin >> n;
+	dp[1] = 1;
+	dp[2] = 2;
+	for (int i = 3; i < n + 1; i++)
+	{
+		dp[i] = (dp[i - 1] + dp[i - 2])%10007;
+	}
+	cout << dp[n];
+
+	return 0;
+}
+
+	
