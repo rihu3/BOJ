35d3787ff7e6dbcf2c922f2cbd1c87c4cd42252a
RIHU <rihu0311@gmail.com>
2024-03-11 04:12:45 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int total;
+	cin >> total;
+	int n;
+	cin >> n;
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		int prcie, EA;
+		cin >> prcie >> EA;
+		sum += prcie * EA;
+	}
+
+	if(total == sum) 
+		cout << "Yes";
+	else
+		cout << "No";
+	return 0;
+}
