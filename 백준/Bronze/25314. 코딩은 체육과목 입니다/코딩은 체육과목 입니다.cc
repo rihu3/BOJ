82b2319bc332a1da00af5ca6e81110b822d9029b
RIHU <rihu0311@gmail.com>
2024-03-11 04:15:38 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	int count = N / 4;
+	for (int i = 0; i < count; i++)
+	{
+		cout << "long ";
+	}
+	cout << "int";
+	return 0;
+}
