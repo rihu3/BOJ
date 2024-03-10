b015cbc9ead40ecb39150dcef4f16b25ed0828b2
RIHU <rihu0311@gmail.com>
2024-03-11 04:08:51 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a == b and b == c and c == a) cout << 10000 + a * 1000;
+	else {
+		if ( a == b ) cout << 1000 + a * 100;
+		else if ( a == c ) cout << 1000 + a * 100;
+		else if ( b == c ) cout << 1000 + b * 100;
+		else {
+			int big;
+			big = a > b ? a : b;
+			big = big > c ? big : c;
+			cout << big * 100;
+		}
+	}
+	return 0;
+}
