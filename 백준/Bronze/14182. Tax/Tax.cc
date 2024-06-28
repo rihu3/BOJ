7844ddd9797f4930ea3b7f28377a0e846db9d8b8
RIHU <rihu0311@gmail.com>
2024-06-28 20:13:04 +0900

    [Bronze III] Title: Tax, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14182. Tax/Tax.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int money;
+	long long result;
+	while (true)
+	{
+		cin >> money;
+		if (money == 0) break;
+		
+		if (money <= 1000000)
+			result = money;
+		else if (money <= 5000000)
+			result = money * 0.9;
+		else 
+			result = money * 0.8;
+
+		cout << result << endl;
+	}
+	return 0;
+}
