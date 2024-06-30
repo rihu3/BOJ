09bb494024f650aedc97fe12b18d44a223339ff0
RIHU <rihu0311@gmail.com>
2024-06-30 13:57:04 +0900

    [Bronze III] Title: Not A + B, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23530. Not A ＋ B/Not A ＋ B.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		cout << 1 << endl;
+	}
+
+	return 0;
+}
