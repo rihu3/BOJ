f88d770db160ef53c3010ad14071c06b27bcec93
RIHU <rihu0311@gmail.com>
2024-07-29 18:44:32 +0900

    [Bronze III] Title: 타슈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30018. 타슈/타슈.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	int a[101], b[101];
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+	for (int i = 0; i < n; i++)
+		cin >> b[i];
+
+	int sum = 0;
+	for (int j = 0; j < n; j++)
+	{
+		int dif = a[j] - b[j];
+		if (dif < 0) dif = -1 * dif;
+		sum += dif;
+	}
+	cout << sum / 2 << endl;
+	return 0;
+}
